6f8e725cec4c8b828ce248627fd05c8cfed9a560
CoolPlayLin <CoolPlayLin@qq.com>
2023-07-23 19:40:54 +0800

    Update

---
+++ src/Cpp_Practise/Practise/for1.2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+
+using namespace std;
+
+__declspec(dllexport) int NestedFor()
+{
+	for (int i = 1; i <= 10; i++)
+	{
+		for (int i = 1; i <= 10; i++)
+		{
+			cout << "*";
+		}
+		cout << endl;
+	}
+	return 0;
+}
---
+++ src/Cpp_Practise/Practise/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+extern int desk();
+extern int NestedFor();
+
+int main()
+{
+	desk();
+	NestedFor();
+	return 0;
+}
