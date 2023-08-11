92b6dd846e8048eb225ddeeb125d5e378739e447
CoolPlayLin <CoolPlayLin@qq.com>
2023-08-11 16:25:47 +0800

    Starter Vue

--- src/Vue_Practise/src/App.vue
+++ src/Vue_Practise/src/App.vue
@@ -1,51 +1,9 @@
 <script setup>
-import HelloWorld from "./components/HelloWorld.vue";
-import TheWelcome from "./components/TheWelcome.vue";
+const msg = "Hello World -- This is the Starter of Your Dream";
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="./assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper"></div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <p>{{ msg }}</p>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
